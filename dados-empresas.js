6edfc353565f060d217a4095f3132423e2945230
mpconsultoriadp <mp.consultoriadp@gmail.com>
2026-04-04 18:18:29 -0300

    Update dados-empresas.js

--- dados-empresas.js
+++ dados-empresas.js
@@ -2,88 +2,88 @@ const dadosEmpresas = {
   "1": {
     nome: "Cupim Grill",
     logo: "/sasled/logotipo/cupim-grill.png",
-    whatsapp: "https://wa.me/5534999999991",
-    instagram: "#",
+    whatsapp: "https://wa.me/5534998814691",
+    instagram: "https://www.instagram.com/cupimgrillchurrascaria/",
     segmento: "Restaurante",
     descricao: "Especializado em carnes e experiência gastronômica."
   },
   "2": {
     nome: "Grife ABCZ",
     logo: "/sasled/logotipo/grife-abcz.png",
-    whatsapp: "https://wa.me/5534999999992",
-    instagram: "#",
+    whatsapp: "https://wa.me/5534996617441",
+    instagram: "https://www.instagram.com/grifeabcz/",
     segmento: "Moda",
     descricao: "Loja oficial da ABCZ com produtos exclusivos."
   },
   "3": {
     nome: "ABCZ",
     logo: "/sasled/logotipo/abcz.png",
-    whatsapp: "https://wa.me/5534999999993",
-    instagram: "#",
+    #whatsapp: "https://wa.me/5534999999993",#
+    instagram: "https://www.instagram.com/abcz.pmgz/",
     segmento: "Agro",
     descricao: "Associação Brasileira dos Criadores de Zebu."
   },
   "4": {
     nome: "Universidade do Agro",
     logo: "/sasled/logotipo/uni-agro.png",
-    whatsapp: "https://wa.me/5534999999994",
-    instagram: "#",
+    #whatsapp: "https://wa.me/553433342212",#
+    instagram: "https://www.instagram.com/universidade.agro/",
     segmento: "Educação",
     descricao: "Cursos e capacitação para o agronegócio."
   },
   "5": {
     nome: "Auto Escola Rezende",
     logo: "/sasled/logotipo/rezende.png",
-    whatsapp: "https://wa.me/5534999999995",
-    instagram: "#",
+    whatsapp: "https://wa.me/553433342212",
+    instagram: "https://www.instagram.com/autoescolarezende/",
     segmento: "Serviços",
     descricao: "Formação de condutores."
   },
   "6": {
     nome: "Alta Genetics",
     logo: "/sasled/logotipo/alta.png",
-    whatsapp: "https://wa.me/5534999999996",
-    instagram: "#",
+    #whatsapp: "https://wa.me/5534999999996",#
+    instagram: "https://www.instagram.com/altabrasiloficial/?hl=pt-br",
     segmento: "Genética",
     descricao: "Soluções genéticas para pecuária."
   },
   "7": {
     nome: "Genex Brasil",
     logo: "/sasled/logotipo/genex.png",
-    whatsapp: "https://wa.me/5534999999997",
-    instagram: "#",
+    whatsapp: "https://wa.me/5516992653019",
+    instagram: "https://www.instagram.com/genexbrasil/",
     segmento: "Genética",
     descricao: "Tecnologia e inovação em genética animal."
   },
   "8": {
     nome: "FAZU",
     logo: "/sasled/logotipo/fazu.png",
-    whatsapp: "https://wa.me/5534999999998",
-    instagram: "#",
+    whatsapp: "https://wa.me/553433184188",
+    instagram: "https://www.instagram.com/fazuonline/",
     segmento: "Educação",
     descricao: "Faculdade de Zootecnia e agronegócio."
   },
   "9": {
     nome: "Girolando",
     logo: "/sasled/logotipo/girolando.png",
-    whatsapp: "https://wa.me/5534999999999",
-    instagram: "#",
+    whatsapp: "https://wa.me/553433316032",
+    instagram: "https://www.instagram.com/associacaogirolando/",
     segmento: "Agro",
     descricao: "Associação da raça Girolando."
   },
   "10": {
     nome: "GWM Motors",
     logo: "/sasled/logotipo/gwm.png",
-    whatsapp: "https://wa.me/5534999999910",
-    instagram: "#",
+    #whatsapp: "https://wa.me/5534999999910",#
+    instagram: "https://www.instagram.com/gwmbrasil/",
     segmento: "Automotivo",
     descricao: "Montadora global de veículos."
   },
   "11": {
     nome: "Coltello Beef",
     logo: "/sasled/logotipo/coltello.jpg",
-    whatsapp: "https://wa.me/5534999999911",
-    instagram: "#",
+    whatsapp: "https://wa.me/5534999288400",
+    instagram: "https://www.instagram.com/coltellobeef/",
     segmento: "Alimentos",
     descricao: "Carnes premium e cortes especiais."
   }
